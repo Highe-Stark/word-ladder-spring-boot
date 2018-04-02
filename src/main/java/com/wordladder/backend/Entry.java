81a758ba471adb5453816ef52cbe3d3f07cfb9de
Higher Stark <higherwashington@gmail.com>
2018-04-02 18:58:12 +0800

    Log added

--- src/main/java/com/wordladder/backend/Entry.java
+++ src/main/java/com/wordladder/backend/Entry.java
@@ -1,8 +1,9 @@
 package com.wordladder.backend;
 import java.util.*;
-import java.nio.file.Paths;
 import java.io.*;
-import java.net.URL;
+//import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,41 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Entry
 {
+	 static Logger log = LoggerFactory.getLogger(Entry.class);
+
 	@RequestMapping("/wordladder")
 	public String main(@RequestParam(value="_beg", defaultValue="word") String _beg, @RequestParam(value="_end", defaultValue="path") String _end)
 	{
 		String beg = _beg;		// the word to begin in the word ladder
 		String end = _end;		// the destination word of the ladder
 		HashSet<String> dict = new HashSet<String>();	//the dictionary
-		// ClassLoader classloader = getClass().getClassLoader();
-		// File dict = classloader.getResource("static/dictionary-1.txt");
 		String userdir = System.getProperty("user.dir");
-		//System.out.println(userdir);
 		String dictPath = userdir + "\\src\\main\\resources\\static\\dictionary-1.txt";
-		/*boolean hasArgs = false;  // no arguments in args
-
-		if (args.length != 0) {
-			String help = "Usage:\n  -d <path> \n\tPath of the dictionary file.\n  -b <word> \n\tthe word where the ladder starts.\n  -e <word> \n\tthe destination word.\n";
-			if (args.length != 6) {
-				System.out.println("Invalid paramaters\n");
-				System.out.println(help);
-				return;
-			}
-			if (!(args[0].equals("-d") && args[2].equals("-b") && args[4].equals("-e"))) {
-				System.out.println("Invalid paramaters\n" + help);
-				return;
-			}
-			dictPath = args[1];
-			beg = args[3];
-			end = args[5];
-			hasArgs = true;
-		}*/
-
-		/* Scanner in  = new Scanner(System.in);
-		if (!hasArgs) {
-			System.out.print("Dictionary file path: ");
-			dictPath = in.nextLine();
-		}*/
 		Scanner dictFile;
 		try {
 		dictFile = new Scanner(new File(dictPath), "UTF-8");
@@ -62,15 +38,17 @@ public class Entry
 		}
 		try {
 			WordLadder wl = new WordLadder(beg, end, dict);
+			log.info("Request: " + beg + " -> " + end);
 			return (wl.findLadder());
 		}
 		catch (NullPointerException e){
+			log.error("Error: Null Pointer exception");
 			return (e.getMessage());
 		}
 		catch (IllegalArgumentException e) {
+			log.error("Error: Illegal Arguments");
 			return (e.getMessage());
 		}
 	}
 
-	// private static String[] parseArgs(String[] a
 }
